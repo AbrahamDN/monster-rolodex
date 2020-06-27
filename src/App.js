0daece1ab5f8175be12cda86d89c6f613e901511
AbrahamDN <abe.ndinga@gmail.com>
2020-06-27 13:26:41 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [
+        {
+          name: 'Loading...',
+          id: '1'
+        }
+      ],
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(users => this.setState({monsters: users}))
+  }
+
+  render() {
+    return (
+      <div className='App'>
+
+        {
+          this.state.monsters.map(monster => <h1 key={monster.id}>{monster.name}</h1>)
+        }
+
+      </div>
+    );
+  }
+}
+
+export default App;
