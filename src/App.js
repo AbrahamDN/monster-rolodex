142bf42a6da8ac7bfbbda81609de1425ed4025e4
AbrahamDN <abe.ndinga@gmail.com>
2020-06-29 08:44:50 +0100

    +Search field

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
           id: '1',
         },
       ],
+      searchField: '',
     };
   }
 
@@ -25,6 +26,11 @@ class App extends Component {
   render() {
     return (
       <div className='App'>
+        <input
+          type='search'
+          placeholder='search monsters'
+          onChange={(e) => this.setState({ searchField: e.target.value })}
+        />
         <CardList monsters={this.state.monsters}></CardList>
       </div>
     );
