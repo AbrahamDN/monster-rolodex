e83279a1944bbca6f2a56420b5fa59439857b468
AbrahamDN <abe.ndinga@gmail.com>
2020-06-29 14:40:53 +0100

    Updates

---
+++ precache-manifest.b191241004b09806a801743c67fe16ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e37b1aaeda6951fda2747430c4da1ee",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "2040862f7b8124a225bc",
+    "url": "/monster-rolodex/static/css/main.da87b36e.chunk.css"
+  },
+  {
+    "revision": "1bb5dd2a6657ff2ee685",
+    "url": "/monster-rolodex/static/js/2.b6f2604e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.b6f2604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2040862f7b8124a225bc",
+    "url": "/monster-rolodex/static/js/main.00d53bed.chunk.js"
+  },
+  {
+    "revision": "90506cc279e62bb14608",
+    "url": "/monster-rolodex/static/js/runtime-main.59621b72.js"
+  }
+]);
